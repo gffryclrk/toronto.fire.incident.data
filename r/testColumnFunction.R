cef2471edeb51af9b4e9af22e66c3c3b7a714d82
gffryclrk <mr@gffryclrk.com>
2018-06-24 08:47:33 -0400

    Updated DataPrep. Finding relationship among features

--- r/testColumnFunction.R
+++ r/testColumnFunction.R
@@ -1,8 +1,7 @@
 testColumn <- function(x){
-  # browser()
-  os_time <- paste(as.Date(x$DISPATCH_DATE)," ",formatC(x$ONSCENE_HOUR, width=2, flag="0"), ":", formatC(x$ONSCENE_MIN, width=2,flag="0"), ":",formatC(x$ONSCENE_SEC, width=2, flag="0"), sep="")
-  # cat(os_time, x$DISPATCH_DATE)
-  # cat(class(x$DISPATCH_DATE), class(os_time))
-  # cat(as.POSIXct(as.numeric(x$DISPATCH_DATE), origin="1970-01-01"))
+  # os_time <- paste(as.Date(x$DISPATCH_DATE)," ",formatC(x$ONSCENE_HOUR, width=2, flag="0"), ":", formatC(x$ONSCENE_MIN, width=2,flag="0"), ":",formatC(x$ONSCENE_SEC, width=2, flag="0"), sep="")
+  browser()
+  # os_time <- paste0(x["DISPATCH_DATE"], " ", x["ONSCENE_HOUR"], ":", x["ONSCENE_MIN"], ":", x["ONSCENE_SEC"] )
+  os_time <- paste0(substr(x["DISPATCH_DATE"], 0, 10), " ", x["ONSCENE_HOUR"], ":", x["ONSCENE_MIN"], ":", x["ONSCENE_SEC"] )
   return(as.POSIXct(os_time, format="%Y-%m-%d %H:%M:%S") == x$DISPATCH_DATE)
 }

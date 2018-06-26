c4b8adfb7b01feb86294389abf82dec434738670
gffryclrk <mr@gffryclrk.com>
2018-06-26 15:40:48 -0400

    Updated Data Dictionary, feature selection & cross tables

--- r/tta.R
+++ r/tta.R
@@ -1,13 +1,13 @@
 tta <- function(x){
-  
+  browser()
   if(sum(is.na(c(x["INCIDENT_DATE"], x["INITIAL_CALL_HOUR"], x["INITIAL_CALL_MIN"], x["INITIAL_CALL_SEC"], x["ONSCENE_HOUR"],x["ONSCENE_MIN"],x["ONSCENE_SEC"]))) > 0) return(NA)
-  onscene_date <- x["INCIDENT_DATE"]
+  # onscene_date <- x["INCIDENT_DATE"]
   ic <- as.POSIXct(paste0(
     x["INCIDENT_DATE"], " ", x["INITIAL_CALL_HOUR"], ":", x["INITIAL_CALL_MIN"], ":", x["INITIAL_CALL_SEC"] 
-  ), format="%Y-%m-%d %H:%M:%S")
+  ), format="%Y-%m-%d %H:%M:%S", tz="EST")
   os <- as.POSIXct(paste0(
     x["INCIDENT_DATE"], " ", x["ONSCENE_HOUR"], ":", x["ONSCENE_MIN"], ":", x["ONSCENE_SEC"]
-  ), format="%Y-%m-%d %H:%M:%S")
+  ), format="%Y-%m-%d %H:%M:%S", tz="EST")
   if(x["ONSCENE_HOUR"] < x["INITIAL_CALL_HOUR"]){ os <- os + (24 * 60 * 60) }
   return(as.numeric(os) - as.numeric(ic))
   
